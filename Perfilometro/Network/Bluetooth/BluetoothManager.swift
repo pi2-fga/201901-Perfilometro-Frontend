de94a1e2fc6bbd75a72dafac9a07c651c3978fda
Miguel Pimentel <miguelpimentel2013@gmail.com>
2019-05-10 00:42:09 -0300

    Added Architecture Structure

---
+++ Perfilometro/Network/Bluetooth/BluetoothManager.swift
@@ -0,0 +1,15 @@
+//
+//  BluetoothManager.swift
+//  Perfilometro
+//
+//  Created by Miguel Pimentel on 09/05/19.
+//  Copyright © 2019 Miguel Pimentel. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+
+class BluetoothManager {
+    
+}
---
+++ Perfilometro/Network/Bluetooth/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  Perfilometro
+//
+//  Created by Miguel Pimentel on 10/05/19.
+//  Copyright © 2019 Miguel Pimentel. All rights reserved.
+//
+
+import Foundation
+
+
+class NetworkManager {
+    
+}
