953003d532a8ae86d85fa5860d26176801eeefb8
Miguel Pimentel <miguelpimentel2013@gmail.com>
2019-05-19 23:28:35 -0300

    Added network layer

---
+++ Perfilometro/Network/HTTP/HTTPNetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  HTTPNetworkManager.swift
+//  Perfilometro
+//
+//  Created by Miguel Pimentel on 19/05/19.
+//  Copyright © 2019 Miguel Pimentel. All rights reserved.
+//
+
+import Foundation
