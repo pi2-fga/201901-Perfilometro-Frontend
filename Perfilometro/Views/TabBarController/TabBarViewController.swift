4b467feea3080bd5e088b5267412f1460f06a52d
GeovanniOj <geovannirock@gmail.com>
2019-05-22 18:26:48 -0300

    Refinements of the new Views of the project has been done

--- Perfilometro/Views/TabBarController/TabBarViewController.swift
+++ Perfilometro/Views/TabBarController/TabBarViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.selectedIndex = 1
     }
 
 
