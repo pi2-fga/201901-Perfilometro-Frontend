f39d3c8eb8ac73976111d4cb5ebec5ecc69a4fe6
Miguel Pimentel <miguelpimentel2013@gmail.com>
2019-07-04 20:42:11 -0300

    Added minimal changes

--- Perfilometro/Scenes/Calibrate/CallibrateSensorsTableViewController.swift
+++ Perfilometro/Scenes/Calibrate/CallibrateSensorsTableViewController.swift
@@ -11,78 +11,71 @@ import UIKit
 class CallibrateSensorsTableViewController: UITableViewController {
     
     // MARK: - Outlets
+    
+    @IBOutlet weak var sensor1: UITableViewCell!
+    @IBOutlet weak var sensor2: UITableViewCell!
+    @IBOutlet weak var sensor3: UITableViewCell!
+    @IBOutlet weak var sensor4: UITableViewCell!
+    @IBOutlet weak var sensor5: UITableViewCell!
+    @IBOutlet weak var sensor6: UITableViewCell!
+    @IBOutlet weak var sensor7: UITableViewCell!
+    @IBOutlet weak var sensor8: UITableViewCell!
+    @IBOutlet weak var sensor9: UITableViewCell!
+    @IBOutlet weak var sensor10: UITableViewCell!
+    
 
-    @IBOutlet weak var sensor1: AnimatedSwitch!
-
+    @IBOutlet weak var sensorStateSwitch: UISwitch!
+    
+    // MARK: - Variables
+    
+    var sensors: [UITableViewCell] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupLayout()
-
     }
     
+    @IBAction func changeSensorState(_ sender: Any) {
+        if sensorStateSwitch.isOn {
+            showSensorCells()
+        } else {
+            hideSensorCells()
+        }
+    }
+    
+    
     private func setupLayout() {
-        sensor1.shape = .round
-
+        setupCells()
+    }
+    
+    private func hideSensorCells() {
+        for sensor in sensors {
+            UIView.animate(withDuration: 0.2) {
+                sensor.contentView.isHidden = true
+            }
+        }
+    }
+    
+    private func showSensorCells() {
+        for sensor in sensors {
+            UIView.animate(withDuration: 0.2) {
+                sensor.contentView.isHidden = false
+            }
+        }
     }
 
-    // MARK: - Table view data source
-
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    private func setupCells() {
+    sensors = [ sensor1,
+                sensor2,
+                sensor3,
+                sensor4,
+                sensor5,
+                sensor6,
+                sensor7,
+                sensor8,
+                sensor9,
+                sensor10]
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
