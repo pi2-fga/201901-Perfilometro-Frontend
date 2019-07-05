24efa2d7ce55aa29eacb3d394b661f31d46599de
GeovanniOj <geovannirock@gmail.com>
2019-07-04 22:37:16 -0300

    Addiction of the connectiion of the app to the MQTT Broker

--- Perfilometro/Scenes/Calibrate/CallibrateSensorsTableViewController.swift
+++ Perfilometro/Scenes/Calibrate/CallibrateSensorsTableViewController.swift
@@ -13,25 +13,35 @@ class CallibrateSensorsTableViewController: UITableViewController {
     // MARK: - Outlets
     
     @IBOutlet weak var sensorStateSwitch: UISwitch!
+    @IBOutlet weak var connectToRaspberry: UISwitch!
     
     // MARK: - Variables
     
     var sensors: [UITableViewCell] = []
+    var mQTTManager: MQTT_Manager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.mQTTManager = MQTT_Manager()
+        
         setupLayout()
     }
     
     @IBAction func changeSensorState(_ sender: Any) {
         if sensorStateSwitch.isOn {
-           
+           self.mQTTManager?.calibrateSensors()
         } else {
            
         }
     }
     
+    @IBAction func connectToRaspberryPi(_ sender: Any) {
+        if connectToRaspberry.isOn {
+            self.mQTTManager?.connectToRaspberry()
+        } else {
+            self.mQTTManager?.disconnectToRaspberry()
+        }
+    }
     
     private func setupLayout() {
       
